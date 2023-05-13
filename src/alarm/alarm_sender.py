e1cad67b7a6af870edb9ec6e6c77c8cce1fac467
mike-jianxin-mo <mike.mo.jianxin@gmail.com>
2023-05-13 15:50:09 +1000

    feat: move email template setting out of functions.

--- src/alarm/alarm_sender.py
+++ src/alarm/alarm_sender.py
@@ -1,32 +1,16 @@
 import boto3
 from botocore.exceptions import BotoCoreError, ClientError
+from config import EMAIL_CONFIG
 
-def alarm_email_handler(event, context):
+def lambda_handler(event, context):
     ses = boto3.client('ses')
 
     try:
-        response = ses.send_email(
-            Source='sender@example.com',
-            Destination={
-                'ToAddresses': [
-                    'recipient@example.com',
-                ],
-            },
-            Message={
-                'Subject': {
-                    'Data': 'Test Email',
-                },
-                'Body': {
-                    'Text': {
-                        'Data': 'Test Email Body',
-                    },
-                },
-            },
-        )
+        response = ses.send_email(**EMAIL_CONFIG)
     except (BotoCoreError, ClientError) as error:
         print(error)
         return "Email not sent"
     
-    print("Email sent! Message ID:"),
+    print("Email sent! Message ID:")
     print(response['MessageId'])
     return "Email sent!"
---
+++ src/alarm/config.py
@@ -0,0 +1,18 @@
+EMAIL_CONFIG = {
+    "Source": 'sender@example.com',
+    "Destination": {
+        "ToAddresses": [
+            'recipient@example.com',
+        ]
+    },
+    "Message": {
+        "Subject": {
+            "Data": 'Test Email',
+        },
+        "Body": {
+            "Text": {
+                "Data": 'Test Email Body',
+            }
+        }
+    }
+}
